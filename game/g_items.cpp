3fb6484064bff7dcdb401cda47dead0eab618e52
Setrilo <49916512+Setrilo@users.noreply.github.com>
2020-01-24 20:42:03 -0800

    Added Gunnery System Definitions

--- game/g_items.cpp
+++ game/g_items.cpp
@@ -2744,6 +2744,12 @@ void ClearRegisteredItems( void ) {
 	RegisterItem( BG_FindItemForWeapon( WP_STUN_BATON ) );
 	RegisterItem( BG_FindItemForWeapon( WP_MELEE ) );
 	RegisterItem( BG_FindItemForWeapon( WP_SABER ) );
+	
+	//[Gunnery System]
+	RegisterItem( BG_FindItemForWeapon( WP_BLASTER ) );
+	RegisterItem( BG_FindItemForWeapon( WP_THERMAL ) );
+	RegisterItem( BG_FindItemForWeapon( WP_ROCKET_LAUNCHER ) );
+	//[End Gunnery System]
 
 	if ( level.gametype == GT_SIEGE ) { //kind of cheesy, maybe check if siege class with disp's is gonna be on this map too
 		G_PrecacheDispensers();
